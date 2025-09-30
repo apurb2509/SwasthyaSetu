54f3558690e3e501111fc9247c10ef8bac800605
Apurb Susobhit Baba <itsapurb@gmail.com>
2025-09-30 15:55:36 +0530

    Chatbot and Whatsapp Icon sticky placement on Homepage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
@@ -9,44 +10,54 @@ import PrivacyPolicy from './pages/PrivacyPolicy';
 import TermsOfUse from './pages/TermsOfUse';
 import Disclaimer from './pages/Disclaimer';
 import ProfilePage from './pages/ProfilePage';
-import DefaultLayout from './components/DefaultLayout'; // <-- Import the new layout
+import DefaultLayout from './components/DefaultLayout';
+
+// --- NEW IMPORTS ---
+import SignupPrompt from './components/SignupPrompt';
+import FloatingActionButtons from './components/FloatingActionButtons';
 
 function App() {
   return (
-    <Routes>
-      {/* --- Routes that WILL have a footer --- */}
-      <Route element={<DefaultLayout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/about-us" element={<AboutUs />} />
-        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-        <Route path="/terms-of-use" element={<TermsOfUse />} />
-        <Route path="/disclaimer" element={<Disclaimer />} />
-        <Route 
-          path="/profile" 
+    <>
+      <Routes>
+        {/* --- Routes that WILL have a footer --- */}
+        <Route element={<DefaultLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/about-us" element={<AboutUs />} />
+          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+          <Route path="/terms-of-use" element={<TermsOfUse />} />
+          <Route path="/disclaimer" element={<Disclaimer />} />
+          <Route 
+            path="/profile" 
+            element={
+              <ProtectedRoute>
+                <ProfilePage />
+              </ProtectedRoute>
+            } 
+          />
+        </Route>
+
+        {/* --- Route for Chat Page that WILL NOT have a footer --- */}
+        <Route
+          path="/chat"
           element={
             <ProtectedRoute>
-              <ProfilePage />
+              <div className="flex flex-col min-h-screen bg-gray-50 font-sans">
+                <Navbar />
+                <main className="flex-grow">
+                  <Chat />
+                </main>
+              </div>
             </ProtectedRoute>
-          } 
+          }
         />
-      </Route>
+      </Routes>
 
-      {/* --- Route for Chat Page that WILL NOT have a footer --- */}
-      <Route
-        path="/chat"
-        element={
-          <ProtectedRoute>
-            <div className="flex flex-col min-h-screen bg-gray-50 font-sans">
-              <Navbar />
-              <main className="flex-grow">
-                <Chat />
-              </main>
-            </div>
-          </ProtectedRoute>
-        }
-      />
-    </Routes>
+      {/* --- NEW GLOBAL COMPONENTS --- */}
+      <SignupPrompt />
+      <FloatingActionButtons />
+    </>
   );
 }
 
